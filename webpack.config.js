0f2cede2f98c89a846f64684a2493ed5b54b0f1a
Joe Barnett <joebarnett876@gmail.com>
2019-02-05 10:17:14 +0000

    added SCSS support

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,23 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                postcssPresetEnv(/* pluginOptions */)
+              ],
+            }
+          },
+          'sass-loader'
+        ],
+      },
     ]
   },
 };
