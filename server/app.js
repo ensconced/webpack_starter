ee3cb15d89df6e4be8bcd242d1b08c48a50762fd
Joe Barnett <joebarnett876@gmail.com>
2019-02-05 14:42:41 +0000

    set up server with heroku

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const dist = path.join(__dirname, '../dist')
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(dist));
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
