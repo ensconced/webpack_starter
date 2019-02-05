1e8b6112b5542a1f7d7bf652f7d60506b13767de
Joe Barnett <joebarnett876@gmail.com>
2019-02-05 14:51:21 +0000

    fix port ReferenceError

--- server/app.js
+++ server/app.js
@@ -4,4 +4,5 @@ const app = express();
 const dist = path.join(__dirname, '../dist')
 
 app.use(express.static(dist));
-app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${port}`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
