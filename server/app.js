13f1aeb38dc4cc4d71fb29b352eb520c1d321fec
Joe Barnett <joebarnett876@gmail.com>
2019-02-05 14:48:33 +0000

    fix port for production

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
 const dist = path.join(__dirname, '../dist')
 
 app.use(express.static(dist));
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${port}`));
